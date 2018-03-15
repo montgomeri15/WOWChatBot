01d7ac648591fd7e5a09f41aeddcaf317bb91551
montgomeri15 <montgomeri15@gmail.com>
2018-03-15 23:52:52 +0200

    With createTable().

--- src/com/company/DB/DbManager.java
+++ src/com/company/DB/DbManager.java
@@ -11,10 +11,24 @@ public class DbManager {
     public static ResultSet resultSet;  //ResultSet содержит все строки и предоставляет доступ к данным в этих строках.
 
     /** Подключение к БД */
-    public Connection getConnection() throws ClassNotFoundException, SQLException {
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.jdbc.Driver");  //Class.forName() приводит к загрузке класса и инициализации его статической части.
         connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/location", "root", "root");
         System.out.println("Установлено соединение с БД.");
         return connection;
+    }
+
+    /** Создание таблицы */
+    public static void createTable() throws Exception {
+        try {
+            connection = getConnection();
+            PreparedStatement ps_create = connection.prepareStatement("CREATE TABLE IF NOT EXISTS tablename(id INT NOT NULL AUTO_INCREMENT, ourkey VARCHAR(255), value VARCHAR(255), PRIMARY KEY(id))");
+            ps_create.executeUpdate();
+        } catch (Exception e){
+            System.out.println(e);
+        }
+        finally {
+            System.out.println("Создание таблицы завершено.");
+        }
     }
 }
