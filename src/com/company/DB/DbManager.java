5f6c3e135bf97dc98486526a49196cf83da81d2e
montgomeri15 <montgomeri15@gmail.com>
2018-03-26 22:59:30 +0300

    And the get() method will be with try...catch.

--- src/com/company/DB/DbManager.java
+++ src/com/company/DB/DbManager.java
@@ -18,16 +18,11 @@ public class DbManager {
 
     /** Создание таблицы */
     public static void createTable() throws ClassNotFoundException, SQLException {
-        try {
-            connection = getConnection();
-            ps = connection.prepareStatement("CREATE TABLE IF NOT EXISTS chatbot(id INT PRIMARY KEY AUTO_INCREMENT, our_key VARCHAR(255), our_value VARCHAR(255))");
-            ps.executeUpdate();
-        } catch (Exception e){
-            System.out.println(e);
-        }
-        finally {
-            System.out.println("Создание таблицы завершено.");
-        }
+        connection = getConnection();
+        ps = connection.prepareStatement("CREATE TABLE IF NOT EXISTS chatbot(id INT PRIMARY KEY AUTO_INCREMENT, our_key VARCHAR(255), our_value VARCHAR(255))");
+        ps.executeUpdate();
+
+        System.out.println("Создание таблицы завершено.");
     }
 
     /** Добавление в таблицу информации */
@@ -35,19 +30,13 @@ public class DbManager {
         String hello_key = "привет";
         String hello_value = "hello";
 
-        try {
-            connection = getConnection();
-            ps = connection.prepareStatement("INSERT INTO chatbot(our_key, our_value) VALUES ('"+hello_key+"','"+hello_value+"')");
-            ps.executeUpdate();
-        } catch (Exception e){
-            System.out.println(e);
-        } finally {
-            System.out.println("Таблица заполнена.");
-        }
+        connection = getConnection();
+        ps = connection.prepareStatement("INSERT INTO chatbot(our_key, our_value) VALUES ('"+hello_key+"','"+hello_value+"')");
+        ps.executeUpdate();
     }
 
     /** Выбор и вывод данных */
-    public static ArrayList<String> get() throws ClassNotFoundException, SQLException {
+    public static ArrayList<String> get() {
         try {
             connection = getConnection();
             ps = connection.prepareStatement("SELECT our_key, our_value FROM chatbot");
